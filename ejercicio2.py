84d887a6e276a17932f483f33474e313ba7e57db
kersaki <marieteceu@gmail.com>
2022-09-22 19:05:40 +0200

    ejercicios

---
+++ ejercicio2.py
@@ -0,0 +1,11 @@
+numero_magico = 12345679
+print(numero_magico)
+numero_usuario = int(input('Introduce un numero del 1 al 9: '))
+print('Has elegido el', numero_usuario)
+numero_por_9 = numero_usuario*9
+print('Tu numero multiplicado por 9 es:', numero_por_9)
+numero_final = numero_por_9*numero_magico
+print('El valor dinal es de:', numero_final)
+
+
+
---
+++ ejercicio6.py
@@ -0,0 +1,16 @@
+
+lista=[]
+numeros=int(input('Introduzca los numeros deseados: '))
+lista.append(numeros)
+print(lista)
+lista_pares=[]
+lista_imp=[]
+if numeros in lista % 2:
+    lista_pares.append()
+else:
+    lista_imp.append()
+print(lista_pares)
+print(lista_imp)
+
+
+
