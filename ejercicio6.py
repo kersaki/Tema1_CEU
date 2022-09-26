aab58d27b0010d0e8374210171f781bdf0cfd5ae
kersaki <marieteceu@gmail.com>
2022-09-26 11:15:14 +0200

    continuacion

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,8 +1,10 @@
-lista=[input('Introduzca los numeros deseados: ')]
+lista=[int(input('Introduzca los numeros deseados: '))]
 lista_pares=[]
 lista_impares=[]
 for i in lista:
-    if i % 2:
+    if i % 2 == 0:
         lista_pares.append(i)
     else:
         lista_impares.append(i)
+print(lista_pares)
+print(lista_impares)
