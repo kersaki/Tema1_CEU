ed4e59c247cee0c418fcf1d222c9db99edd06490
kersaki <marieteceu@gmail.com>
2022-10-03 18:16:46 +0200

    hola

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,5 @@
 nombre=input('Intrduzca el nombre y el apellido: ')
 nota= int(input('Introduuzca la nota: '))
+print('El alumno', nombre, 'ha sacado un', nota)
 cadena=nombre,nota
+print(cadena[::-1])
--- ejercicio4.py
+++ ejercicio4.py
@@ -1,1 +1,5 @@
-
+tareas=['1: introduccion', '4: tarea', '3: preparacion tarea', '2: lluvia de ideas', '5: presentacion final']
+def ordenar_tareas (tareas):
+    tareas.sort()
+    print(tareas)
+ordenar_tareas(tareas)
